31b073a52c80cbf75ba3b4cb247c65f8f31ef77b
o0beaner <o0beaner@gmail.com>
2017-05-17 09:52:30 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 if [ ! -d "$HOME/.yadr" ]; then
+    echo 'let g:session_autosave = '"'yes'" >> ~/.vimrc
     echo "Installing YADR for the first time"
     git clone --depth=1 https://github.com/o0beaner/dotfiles.git "$HOME/.yadr"
     cd "$HOME/.yadr"
