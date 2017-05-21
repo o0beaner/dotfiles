82022f1986a437fd8fd89f9cc7c9d6e730378cdd
o0beaner <o0beaner@gmail.com>
2017-05-21 17:28:16 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ if [ ! -d "$HOME/.yadr" ]; then
     git clone --depth=1 https://github.com/o0beaner/dotfiles.git "$HOME/.yadr"
     cd "$HOME/.yadr"
     [ "$1" = "ask" ] && export ASK="true"
-    rake install
+    rake install --trace
 else
     echo "YADR is already installed"
 fi
