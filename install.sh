2a18b1722f31e4adeea5f5f4662e78b7f87f3417
o0beaner <o0beaner@gmail.com>
2017-05-22 08:48:36 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 if [ ! -d "$HOME/.yadr" ]; then
     echo "Installing YADR for the first time"
-    git clone --depth=1 https://github.com/o0beaner/dotfiles.git "$HOME/.yadr"
+    git clone --depth=1 -b smartos --single-branch https://github.com/o0beaner/dotfiles.git "$HOME/.yadr"
     cd "$HOME/.yadr"
     [ "$1" = "ask" ] && export ASK="true"
     rake install --trace
